1d259531622341298b8ddf3e932afdb148e0272a
Mickey Home <mickey@home.com>
2013-07-20 22:49:37 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Punchclock::Application.config.secret_key_base = 'f9f4d9daea272bebfa8bd6b0bd327af945f5281afe6e8c85614284acde45be9b7a3d486bdc5d1e566533a6650d6f68c928cc827fd5f1417521a25a70cf8d8957'
