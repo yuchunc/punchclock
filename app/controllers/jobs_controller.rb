46e30dfcaa5211a7fe6a03d23959b1cfb572560b
Mickey <poverwhelming@gmail.com>
2013-08-17 09:25:59 +0800

    working on the create function

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -14,7 +14,7 @@ class JobsController < ApplicationController
   end
 
   def create
-    Job.create(params[:job])
+    Job.create(job_params)
   end
 
   def edit
@@ -30,7 +30,17 @@ class JobsController < ApplicationController
 
   private
 
-  def find_job
-    @job = Job.find(params[:id])
-  end
+    def find_job
+      @job = Job.find(params[:id])
+    end
+
+    def job_params
+      params.required(:job).permit(:name, :pay_per_hour,
+                                   :hour_per_week, :started_date,
+                                   :end_date, :company)
+    end
+
+    def company_params
+      params.require(:company).permit(:name, :country, :city, :address )
+    end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,14 @@ class Job < ActiveRecord::Base
   belongs_to :company
   belongs_to :user
   has_many :punchcards
+  before_save :check_company
+
+  private
+    def check_company_company
+      if self.company.name.empty?
+        self.company = nil
+      else
+        self.company = Company.find_or_create_by(name: self.company.name)
+      end
+    end
 end
